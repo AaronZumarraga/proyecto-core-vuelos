82aeb693a4f3ca0937b37188d416f5c5f12318f3
Aaron Zumarraga <aaron.zumarraga@hotmail.com>
2024-01-06 11:39:31 -0500

    Reserva visualizacion

--- server.js
+++ server.js
@@ -67,6 +67,116 @@ app.post('/api/inicio-sesion', (req, res) => {
     });
   });  
 
+
+
+
+
+
+  app.get('/api/obtenerOpcionesOrigen', (req, res) => {
+    const query = 'SELECT DISTINCT Origen FROM Vuelo';
+  
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error al obtener opciones de origen:', error);
+        res.status(500).send('Error al obtener opciones de origen');
+      } else {
+        const opcionesOrigen = results.map(result => result.Origen);
+        res.json(opcionesOrigen);
+      }
+    });
+  });
+
+  app.get('/api/obtenerOpcionesDestino', (req, res) => {
+    const query = 'SELECT DISTINCT Destino FROM Vuelo';
+  
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error al obtener opciones de destino:', error);
+        res.status(500).send('Error al obtener opciones de destino');
+      } else {
+        const opcionesDestino = results.map(result => result.Destino);
+        res.json(opcionesDestino);
+      }
+    });
+  });
+  app.get('/api/obtenerOpcionesHora', (req, res) => {
+    const query = 'SELECT DISTINCT DATE_FORMAT(Hora, "%H:%i") AS Hora FROM Vuelo';
+  
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error al obtener opciones de hora:', error);
+        res.status(500).send('Error al obtener opciones de hora');
+      } else {
+        const opcionesHora = results.map(result => result.Hora);
+        res.json(opcionesHora);
+      }
+    });
+  });
+  app.get('/api/obtenerOpcionesPrecio', (req, res) => {
+    const query = 'SELECT DISTINCT Precio FROM Vuelo';
+  
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error al obtener opciones de precio:', error);
+        res.status(500).send('Error al obtener opciones de precio');
+      } else {
+        const opcionesPrecio = results.map(result => result.Precio);
+        res.json(opcionesPrecio);
+      }
+    });
+  });
+  app.get('/api/obtenerOpcionesAerolinea', (req, res) => {
+    const query = 'SELECT DISTINCT Nombre FROM Aerolinea';
+  
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error al obtener opciones de nombre de Aerolínea:', error);
+        res.status(500).send('Error al obtener opciones de nombre de Aerolínea');
+      } else {
+        const opcionesAerolinea = results.map(result => result.Nombre);
+        res.json(opcionesAerolinea);
+      }
+    });
+  });
+  app.post('/api/hacerReserva', (req, res) => {
+    const { origen, destino, hora, precio, aerolinea, nombreUsuario } = req.body;
+  
+    const sql = 'INSERT INTO Reserva (Origen, Destino, Hora, Precio, Aerolinea, Nombre_Usuario) VALUES (?, ?, ?, ?, ?, ?)';
+    connection.query(sql, [origen, destino, hora, precio, aerolinea, nombreUsuario], (err, result) => {
+      if (err) {
+        console.error('Error al hacer reserva:', err);
+        res.status(500).send('Error al hacer reserva');
+      } else {
+        console.log('Reserva realizada con éxito');
+        res.status(200).json({ message: 'Reserva realizada con éxito' });
+      }
+    });
+  });
+  
+    
+  
+  app.get('/api/obtenerReservas', (req, res) => {
+    const sql = 'SELECT * FROM Reserva';
+  
+    connection.query(sql, (err, results) => {
+      if (err) {
+        console.error('Error al obtener reservas:', err);
+        res.status(500).send('Error al obtener reservas');
+      } else {
+        res.json(results);
+      }
+    });
+  });
+  
+  
+  
+  
+    
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Servidor Node.js en ejecución en el puerto ${port}`);
 });
