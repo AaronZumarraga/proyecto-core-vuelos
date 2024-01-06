0d7dad76d9bb61abf3af97dca92393c1757fdc9c
Aaron Zumarraga <aaron.zumarraga@hotmail.com>
2024-01-05 23:43:02 -0500

    Registro en mysql logrado

--- server.js
+++ server.js
@@ -1,21 +1,48 @@
+const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-// Configuración de la conexión a la base de datos
-const connection = mysql.createConnection({
-    host: 'localhost    ',  // Cambiar por el nombre del host de tu base de datos
-    user: 'root', // Cambiar por el nombre de usuario de tu base de datos
-    password: 'aaron', // Cambiar por la contraseña de tu base de datos
-    database: 'reserva_vuelos_bdd' // Cambiar por el nombre de tu base de datos
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'aaron',
+  database: 'reserva_vuelos_bdd',
+};
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos exitosa');
 });
 
-// Conectar a la base de datos
-connection.connect((error) => {
-    if (error) {
-        console.error('Error de conexión a la base de datos:', error);
+// Endpoint de registro
+app.post('/api/registro', (req, res) => {
+  const { nombre, apellido, correo, telefono, nombreUsuario, contrasena } = req.body;
+
+  // Insertar usuario en la base de datos
+  const sql = 'INSERT INTO Pasajero (Nombre, Apellido, Correo_Electronico, Telefono, Nombre_Usuario, Contrasena) VALUES (?, ?, ?, ?, ?, ?)';
+  connection.query(sql, [nombre, apellido, correo, telefono, nombreUsuario, contrasena], (err, result) => {
+    if (err) {
+      console.error('Error al insertar usuario en la base de datos:', err);
+      res.status(500).send('Error interno del servidor');
     } else {
-        console.log('Conexión exitosa a la base de datos');
+      console.log('Usuario registrado con éxito');
+      res.status(200).send('Usuario registrado con éxito');
     }
+  });
 });
 
-module.exports = connection;
-
+app.listen(port, () => {
+  console.log(`Servidor Node.js en ejecución en el puerto ${port}`);
+});
